7f560071687e2b642087bd1e1d3c4cd6eef3b2b6
hamyareweb <javid.x3@gmail.com>
2022-07-23 23:30:12 +0430

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>Next.js Starter!</title>
+        <title>بهترین برنامه برای حذف فالوور فیک</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
